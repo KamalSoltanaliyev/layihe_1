14fd185083b708a335c3c5eb0017a48ea6e29827
KamalSoltanaliyev <148267364+KamalSoltanaliyev@users.noreply.github.com>
2023-11-28 20:28:39 +0400

    Add files via upload

---
+++ layihe_1/layihe_1/GenericCustomList.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace layihe_1
+{
+    public class GenericCustomList<T>
+    {
+        private T[] items;
+        private int count;
+        private int capacity;
+
+        public int Count
+        {
+            get { return count; }
+        }
+
+        public int Capacity
+        {
+            get { return capacity; }
+        }
+        public GenericCustomList()
+        {
+            items = new T[0];
+            count = 0;
+            capacity = 0;
+        }
+        public void Add(T item)
+        {
+            if (count == capacity)
+            {
+                if (capacity == 0)
+                    capacity = 4;
+                else
+                    capacity *= 2;
+
+                T[] newItems = new T[capacity];
+                for (int i = 0; i < count; i++)
+                {
+                    newItems[i] = items[i];
+                }
+
+                items = newItems;
+            }
+
+            items[count] = item;
+            count++;
+        }
+        public T Find(Predicate<T> match)
+        {
+            return Array.Find(items, match);
+        }
+
+
+
+        public GenericCustomList<T> FindAll(Predicate<T> match)
+        {
+            Array.FindAll<T>(items , match);
+            
+            return this;
+            
+
+        }
+
+        public bool Contains(T item)
+        {
+            return items.Contains(item);
+        }
+
+
+
+        public bool Exists(Predicate<T> match)
+        {
+            if (Array.Exists(items, match))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void Remove(Predicate<T> match)
+        {
+            int newIndex = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (!match(items[i]))
+                {
+                    items[newIndex] = items[i];
+                    newIndex++;
+                }
+            }
+
+            count = newIndex;
+        }
+    }   
+}
---
+++ layihe_1/layihe_1/Program.cs
@@ -0,0 +1,11 @@
+﻿using layihe_1;
+
+GenericCustomList<int> generic = new();
+
+generic.Add(1);
+generic.Add(2);
+generic.Add(3);
+generic.Add(4);
+generic.Add(5);
+Console.WriteLine(generic.Count);
+Console.WriteLine(generic.Capacity);
